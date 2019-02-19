bf7fff033e0ec15d25c3d6a765619e8f90ddd334
Oleg Mullakaev <o.mullakaev@gmail.com>
2019-02-19 21:45:38 +0300

    done 2.1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
+console.log(process.env.INTERVAL);
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method);
+  setTimeout(() => {
+    console.info(`${Date()} - timeout`);
+    clearInterval(interval);
+    res.end(Date());
+  }, process.env.TIMEOUT);
+
+  let interval = setInterval(() => {
+    console.info(`${Date()} - interval`);
+  }, process.env.INTERVAL);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
